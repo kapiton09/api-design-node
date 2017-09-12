c88917b906a11e1195b3fc2f0301f84e23564b6f
Kapil Shrestha <contact@kapilshrestha.com>
2017-09-12 17:54:03 +1200

    Testing

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
@@ -17,13 +17,11 @@ app.use('/lions', lionRouter);
 app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
-    if (err) {
-        console.log(err.message);
-        res.status(500).send(err);
-    }
+  if (err) {
+    res.status(500).send(err);
+  }
 });
 
 
 
-app.listen(4000);
-console.log('on port 4000');
+module.exports = app;
--- server/tigers.js
+++ server/tigers.js
@@ -1,65 +1,58 @@
-var _ = require('lodash');
 var tigerRouter = require('express').Router();
 
 var tigers = [];
 var id = 0;
 
 var updateId = function(req, res, next) {
-    if (!req.body.id) {
-        id++;
-        req.body.id = id + '';
-    }
-    next();
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
 };
 
 tigerRouter.param('id', function(req, res, next, id) {
-    var lion = _.find(tigers, { id: id })
+  var tiger = _.find(tigers, {id: id});
 
-    if (lion) {
-        req.lion = lion;
-        next();
-    } else {
-        res.send();
-    }
+  if (tiger) {
+    req.tiger = tiger;
+    next();
+  } else {
+    res.send();
+  }
 });
 
-tigerRouter.route('/')
-    .get(function(req, res) {
-        res.json(tigers);
-    })
-    .post(updateId, function(req, res) {
-        var tiger = req.body;
+tigerRouter.get('/', function(req, res){
+  res.json(tigers);
+});
 
-        tigers.push(tiger);
+tigerRouter.get('/:id', function(req, res){
+  var tiger = req.tiger;
+  res.json(tiger || {});
+});
 
-        res.json(tiger);
-    });
+tigerRouter.post('/', updateId, function(req, res) {
+  var tiger = req.body;
 
-tigerRouter.route('/:id')
-    .get(function(req, res) {
-        var tiger = req.tiger;
-        res.json(tiger || {});
-    })
-    .delete(function(req, res) {
-        var tiger = _.findIndex(tigers, { id: req.params.id });
-        tigers.splice(tiger, 1);
+  tigers.push(tiger);
 
-        res.json(req.tiger);
-    })
-    .put(function(req, res) {
-        var update = req.body;
-        if (update.id) {
-            delete update.id
-        }
+  res.json(tiger);
+});
 
-        var tiger = _.findIndex(tigers, { id: req.params.id });
-        if (!tigers[tiger]) {
-            res.send();
-        } else {
-            var updatedTiger = _.assign(tigers[tiger], update);
-            res.json(updatedTiger);
-        }
-    });
 
+tigerRouter.put('/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+
+  var tiger = _.findIndex(tigers, {id: req.params.id});
+  if (!tigers[tiger]) {
+    res.send();
+  } else {
+    var updatedtiger = _.assign(tigers[tiger], update);
+    res.json(updatedtiger);
+  }
+});
 
 module.exports = tigerRouter;
